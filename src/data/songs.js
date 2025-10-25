48d11971450e04d14cf3dcad5f24319e909a5646
chechu301001 <chechu301001@gmail.com>
2025-10-26 01:55:17 +0530

    WithLyrics

--- src/data/songs.js
+++ src/data/songs.js
@@ -73,45 +73,235 @@ Thaka thakita, Thaka thakita, Thaka thakita, Thaka thakita\n
 Thakita thakita thakita tha\n`},
   {
     id: 'song-2',
-    title: 'Saaral',
+    title: 'SAARAL',
     context: 'A breath of Fresh air',
     highlightLines: [],
-    lyrics: ``
+    lyrics: `Saalaiyil vizhum mazhai saaralaagume\n
+(The rain drizzles on the road)\n
+Sandhame Isaiyanadhe (the falling sound of rain is music)\n
+\n
+Solayil pudhu malar vaasam veesume (in an oasis a new flower blossoms)\n
+Vaasame Isaiyanadhe (the good smell from the new flower also creates same sensation like music)\n
+\n
+Shruthilayam ondrayinge serndhidum tharunam (here is a union of Shruthi and Layam)\n
+Isaiyenum magavume inbamaai jananam (in such union a baby of music is born)\n
+Idhu manadai thalaattum\n
+maayame (music does the magic of calming the mind)\n
+\n
+Ennil unnai serthen ilamayum vellume (the youth has won by bringing you (music) to myself)\n
+Kannil vazhiyum jeevan kadhaigalum sollume (in such union the tears that roll off my eyes express thousands of emotive stories)\n
+\n
+Unavaai Unarvai\n
+(food, feeling)\n
+Kanavai Ninaivai\n
+(Dream, reality)\n
+Sugamai Valiyai\n
+(Pleasure, pain)\n
+Isaiyendrume vazhi kaattume..\n
+(In any such emotive needs, music only can show us the way and life)\n
+\n
+Shruthilayam ondrayinge serndhidum tharunam (here is a union of Shruthi and Layam)\n
+Isaiyenum magavume inbamaai jananam (in such union a child of music is born)\n
+Idhu manadai thalaattum (music does the magic of calming the mind)\n
+`
   },
   {
     id: 'song-3',
-    title: 'Song Three',
-    context: 'A short intro line for Song Three.',
+    title: 'KALEDHODE',
+    context: 'Zone out in Happiness',
     highlightLines: [],
-    lyrics: ``
+    lyrics: `Kaledhode Naaneega kaledhode\n
+Saaguva haadiya noduttha nadevaga\n
+Haaro hakkiya haadondu seledaaga\n
+(I got lost looking at the beauty of nature while travelling, and while listening to the birds singing)\n
+Manasothe Naa manasothe\n
+Modadha naduvalle Naaneega kulithaaga\n
+Aa karaguva baanannu kandaga\n
+(I lost my mind in happiness when I sat in between the clouds and saw the rainbow)\n
+\n
+Jagada maathanne Naa keladanthe \n
+Manase ee maathu sariye\n
+(I listened to my brain ignoring the outside world, is that right?)\n
+Manadi marukshanavu kaadutthide \n
+Manase ninna maaye sereye \n
+(My mind is confused, and stuck in this prison of thought)\n
+Theli theli baanadaariyalli jaaride \n
+Kaledhode \n
+(However I let it all go by sliding down the sky)\n
+\n
+Kaledhode Naanilli kaledhode\n
+Baananu belaguva chandira bandaaga\n
+Minugo thaareya shringara kandaga\n
+(I got lost when I saw the beautiful moon and the jewellery of the night sky, the stars)\n
+\n
+Manasina goodalli gidavondu maravaagi \n
+(In the nest of our mind, a plant became a tree)\n
+Aa marada neralalli Naa ninthe\n
+(I stood under the shade of that tree)\n
+Marada aa neralalli badukina hannina nemmadiya sihiyannu savide\n
+(I stood under it and ate the sweet fruit of relaxation and life)\n
+\n
+Kaanisada swargavu, Bhuviyale kaanade\n
+(We can see the unseen heaven on earth itself)\n
+Kalpanegu meerida, Aa sukhavu illide\n
+(There is happiness here more than we could imagine)\n
+\n
+Nenapina hore bedada hone\n
+(So we mustn't hold on to the burden of our past)\n
+Baruvudu kare thiliyadu Kone\n
+(We never know when the end is nearing)\n
+Namagenthale migilaagihud ee kshnavidu onde\n
+Jeevisu jeevisu\n
+(The only thing in our hands is the present, so live it)\n
+Theli theli kelo manada madila seride\n
+Kaledhode \n
+(I floated and reached the minds of the listeners)\n
+\n
+Kaledhode Naaneega kaledhode \n
+Saaguva haadiya noduttha nadevaga \n
+\n
+Manasina goodalli gidavondu maravaagi \n
+Aa marada neralalli Naa ninthe\n
+Marada aa neralalli badukina hannina nemmadiya sihiyannu savide\n
+Manasina goodalli gidavondu maravaagi \n
+Aa marada neralalli Naa ninthe\n
+Marada aa neralalli badukina hannina nemmadiya sihiyannu savide\n
+Kaledhode.\n
+\n
+Theli theli baanadaariyalli jaaride\n
+`
   },
   {
     id: 'song-4',
-    title: 'Song Four',
-    context: 'A short intro line for Song Four.',
+    title: 'AANDHI',
+    context: 'Control the darkness within',
     highlightLines: [],
-    lyrics: ``
+    lyrics: `Dekho kitne alag se hai tere rastee\n
+Kya sab paana zaruri hai Tere vaste...\n
+In raahon me ab hum tum\n
+Bas khoye hai kahin\n
+Andhero ke bhanvar mein\n
+doobein hum yahin\n
+\n
+Khudse hi ladtha hai ja raha \n
+mann mera sunta nahi\n
+andhera badhta hai ja raha\n
+mita dega tumko yahin\n
+\n
+umeedein dhundoon bas mai\n
+bhatku mai har kahi\n
+Dekhoon aaine mein\n
+dikhta na sach hai kahin\n
+Uljhanein aisi jaise, Yeh Aandhi\n
+\n
+Dekho kitne alag se hai tere rastee...\n
+janu na kya sahi Hai mere vaste..\n
+khamoshi bhi mein sun saku\n
+uljhan har kahi\n
+Raaton ko bhi mein jaagoon \n
+Sukoon hai nahi\n
+\n
+Khudse hi ladtha hai ja raha \n
+mann mera sunta nahi\n
+andhera badhta hai ja raha\n
+mita dega tumko yahin\n
+\n
+umeedein dhundoon bas mai\n
+bhatku mai har kahi\n
+Sach ho Saamne bhi\n
+Aankhein band ho to dikhta nahi\n
+Uljhanein aisi jaise, Yeh Aandhi re\n
+Yeh Aandhi re\n
+\n
+Hoton pe muskaan mere bhi\n
+Par andar ek ghamsaan hai\n
+Dikhta hai jaise uper se \n
+Kya waisa koi bhi insaan hai\n
+Insaan hai\n
+`
   },
   {
     id: 'song-5',
-    title: 'Song Five',
-    context: 'A short intro line for Song Five.',
+    title: 'SAADHANE',
+    context: 'Conquer music',
     highlightLines: [],
-    lyrics: ``
+    lyrics: `Guri iddu prayathnisadale \n
+Adu bariya kanasu \n
+Guri illada prayathna \n
+Bari kaala harana \n
+Guri prayathnada madhye \n
+Saadhane\n
+`
   },
   {
     id: 'song-6',
-    title: 'Song Six',
-    context: 'A short intro line for Song Six.',
+    title: 'THAANAM',
+    context: 'The guiding light',
     highlightLines: [],
-    lyrics: ``
+    lyrics: `Pal yeh tehre\n
+Pal yeh guzre\n
+Toote jaise Kaanch yeh armaan\n
+\n
+Raahein bhatke\n
+Khwaab yeh bikhre\n
+Chhoote sab Mere yeh nishaan\n
+\n
+Bairi piya bathein begani \n
+Saason thale khoyi kahaani \n
+Naina mere bole udaasi\n
+Kismat kya, baawre \n
+Main jaanu na\n
+\n
+Lehre goonjti hain aangan mein\n
+Khile Phool mann ke baagon mein\n
+Roshni chhayi hai raahon mein\n
+\n
+Udne ko khula aasmaan\n
+Khwabon ki nayi daastaan\n
+Aandhiyaan ho ya fasaan,\n
+Laut aaunga har dafa. \n
+\n
+Hai yeh fasaana,\n
+Ya koi bahaana\n
+Kismat kya, haaye re \n
+Main jaanu na\n
+`
   },
   {
     id: 'song-7',
-    title: 'Song Seven',
-    context: 'A short intro line for Song Seven.',
+    title: 'FAASLA',
+    context: 'The story of fate',
     highlightLines: [],
-    lyrics: ``
+    lyrics: `Yaadein, mere saath chal rahe\n
+Saaye bulaaye, yun hi muskura rahe\n
+\n
+Baadal baras rahe hai\n
+Baahein taras rahi hai\n
+Daaman chodta jaaun \n
+Palkein silaata jaaun \n
+Main toh karwat yeh badal na saku\n
+\n
+Tu mera bana sahaara,\n
+Seh raha main gardishon ka silsila\n
+hua yakeen mere dil ko,\n
+yeh tera karam\n
+ya bharam mera - samjhaa do\n
+\n
+Bhulaake teri har yaadein  \n
+Chal raha hoon aakhir \n
+(main) tere bina zinda hoon..\n
+taqdeer yeh kya phir\n
+\n
+Khwahishon ki dhadkan par\n
+Kyun chhupa sa marham hai,\n
+Mujhko yeh samjha do\n
+\n
+Tu mera bana sahaara,\n
+Seh raha main gardishon ka silsila \n
+hua yakeen mere dil ko,\n
+yeh tera karam\n
+ya bharam mera - samjhaa do\n
+`
   }
 ]
 
